5e68f1885b38129adc03131e17c2b2315c0b6883
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-28 14:23:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,9 +11,10 @@ class Song
     @@all
   end
 
-  def self.new_by_filename(song_name)
-    song = self.new
-    song.name = song_name.split(" - ")
+  def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
     song
   end
 
