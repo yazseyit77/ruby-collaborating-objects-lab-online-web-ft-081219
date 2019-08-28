be8867ab080d6491eb080ae537b33ac92365de62
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-28 13:37:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,20 +1,14 @@
 class Artist
   attr_accessor :name, :songs
-  @@all = []
 
   def initialize(name)
     @name = name
     @songs = []
-    @@all << self
   end
 
   def add_song(song)
     @songs << song
     song.artist.self
-  end
-
-  def self.all
-    @@all
   end
 
   def songs
