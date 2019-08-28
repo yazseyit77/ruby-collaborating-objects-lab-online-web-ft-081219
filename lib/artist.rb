ba6bff327e3058884faab1a4f7d9b22edee210be
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-28 14:25:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,11 +10,12 @@ class Artist
 
   def add_song(song)
     self.songs << song
-  end
-
-  def save
     @@all << self
   end
+  # 
+  # def save
+  #   @@all << self
+  # end
 
   def self.all
     @@all
@@ -24,21 +25,21 @@ class Artist
     Song.all.select {|song| song.artist == self}
   end
 
-  def find_or_create_by_name(name)
-    if self.find(name)
-      self.find(name)
-    else
-      self.create(name)
-    end
-  end
-
-  def self.find(name)
-    self.all.detect{|artist| artist.name == name}
-  end
-
-  def self.create(name)
-    artist = Artist.new(name)
-    artist.save
-    artist
-  end
+  # def find_or_create_by_name(name)
+  #   if self.find(name)
+  #     self.find(name)
+  #   else
+  #     self.create(name)
+  #   end
+  # end
+  # 
+  # def self.find(name)
+  #   self.all.detect{|artist| artist.name == name}
+  # end
+  # 
+  # def self.create(name)
+  #   artist = Artist.new(name)
+  #   artist.save
+  #   artist
+  # end
 end
