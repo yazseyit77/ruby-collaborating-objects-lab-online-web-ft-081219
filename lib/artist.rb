d190a6e1ca7928d5e50c2700ad6876ffc3273f5f
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-28 14:27:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,29 +17,8 @@ class Artist
     @@all
   end
 
-  # def save
-  #   @@all << self
-  # end
-
   def songs
     Song.all.select {|song| song.artist == self}
   end
 
-  # def find_or_create_by_name(name)
-  #   if self.find(name)
-  #     self.find(name)
-  #   else
-  #     self.create(name)
-  #   end
-  # end
-  #
-  # def self.find(name)
-  #   self.all.detect{|artist| artist.name == name}
-  # end
-  #
-  # def self.create(name)
-  #   artist = Artist.new(name)
-  #   artist.save
-  #   artist
-  # end
 end
