2fea60f7c6be45fb10071680d543e1f873c72896
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-28 14:31:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,11 +19,7 @@ class Song
   end
 
   def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
+    self.artist = Artist.find_or_create_by_name(name)
   end
 
 end
