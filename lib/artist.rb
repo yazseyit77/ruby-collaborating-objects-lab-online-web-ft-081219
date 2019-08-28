a1ac4daacbde0b384227738ba21b3efda315ec49
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-28 13:42:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,11 @@
 class Artist
   attr_accessor :name, :songs
+  @@all = []
 
   def initialize(name)
     @name = name
     @songs = []
+    @@all << self
   end
 
   def add_song(song)
@@ -11,6 +13,18 @@ class Artist
     song.artist.self
   end
 
+  def self.all
+    @@all
+  end
+
+  def find_or_create_by_name=(name)
+    if self.artist.nil?
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = (name)
+    end
+  end
+  
   def songs
     Song.all.select{|song| song.artist == self}
   end
